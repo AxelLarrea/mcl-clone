21ee5bca3d57109a060c8c590a33a3b5e5b121b8
AxelLarrea <axel.larrea.9@gmail.com>
2025-05-17 01:53:42 -0300

    update css file imports

--- src/components/product/ProductQuestionsAnswers.jsx
+++ src/components/product/ProductQuestionsAnswers.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import '../../styles/product/ProductQuestionsAnswers.css';
+import '../../styles/product/productQuestionsAnswers.css';
 
 const ProductQuestionsAnswers = ({ questions }) => {
     
